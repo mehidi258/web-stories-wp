5ab64f08e00ca7abe815ff62775cc72634c54c5f
Sayed Taqui <sayedwp@gmail.com>
2021-04-20 17:20:53 +0530

    code cleanup

--- index.php
+++ index.php
@@ -1,21 +1,16 @@
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 	<title>Web Story Ad</title>
-	<link rel='stylesheet' href='https://wordpress.app/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5Bchunk_0%5D=dashicons,admin-bar,common,admin-menu,dashboard,list-tables,edit,revisions,media,themes,about,nav-menus,wp-pointer,widgets,site-&amp;load%5Bchunk_1%5D=icon,l10n,buttons,wp-auth-check,media-views&amp;ver=5.6.3' media='all' />
-	<link rel='stylesheet' id='mediaelement-css'  href='https://wordpress.app/wp-includes/js/mediaelement/mediaelementplayer-legacy.min.css?ver=4.2.16' media='all' />
-	<link rel='stylesheet' id='wp-mediaelement-css'  href='https://wordpress.app/wp-includes/js/mediaelement/wp-mediaelement.min.css?ver=5.6.3' media='all' />
-	<link rel='stylesheet' id='imgareaselect-css'  href='https://wordpress.app/wp-includes/js/imgareaselect/imgareaselect.css?ver=0.9.8' media='all' />
-	<link crossorigin="anonymous" rel='stylesheet' id='google-fonts-css'  crossorigin='anonymous' href='https://fonts.googleapis.com/css?family=Google+Sans%7CGoogle+Sans%3Ab%7CGoogle+Sans%3A500&#038;display=swap&#038;ver=1.7.0-alpha.0' media='all' />
-	<link rel='stylesheet' id='vendors-edit-story-css'  href='https://wordpress.app/wp-content/plugins/web-stories-wp/assets/css/vendors-edit-story.css' media='all' />
-	<link rel='stylesheet' id='vendors-edit-story-rtl-css'  href='https://wordpress.app/wp-content/plugins/web-stories-wp/assets/css/vendors-edit-story-rtl.css' media='all' />
-	<link rel='stylesheet' id='edit-story-rtl-css'  href='https://wordpress.app/wp-content/plugins/web-stories-wp/assets/css/edit-story-rtl.css' media='all' />
-	<link rel='stylesheet' id='edit-story-css'  href='https://wordpress.app/wp-content/plugins/web-stories-wp/assets/css/edit-story.css?ver=d1683a0c1cc0480a1798f1da20f3b018' media='all' />
-	<link rel='stylesheet' id='amp-post-meta-box-css'  href='https://wordpress.app/wp-content/plugins/amp/assets/css/amp-post-meta-box.css?ver=2.0.11' media='all' />
 
-	<script src='https://wordpress.app/wp-admin/load-scripts.php?c=0&amp;load%5Bchunk_0%5D=jquery-core,jquery-migrate,utils&amp;ver=5.6.3'></script>
-	<script src='https://wordpress.app/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4' id='wp-polyfill-js'></script>
+	<!-- https://wordpress.app/wp-admin/load-styles.php?load[chunk_0]=common -->
+	<link rel='stylesheet' href='web-stories-ad/assets/wp-assets/load-style-php.css'/>
+	<link crossorigin="anonymous" rel='stylesheet' crossorigin='anonymous' href='https://fonts.googleapis.com/css?family=Google+Sans%7CGoogle+Sans%3Ab%7CGoogle+Sans%3A500&#038;display=swap&#038;ver=1.7.0-alpha.0'/>
+	<link rel='stylesheet' href='web-stories-wp/assets/css/vendors-edit-story.css'/>
+	<link rel='stylesheet' href='web-stories-wp/assets/css/vendors-edit-story-rtl.css'/>
+	<link rel='stylesheet' href='web-stories-wp/assets/css/edit-story-rtl.css'/>
+	<link rel='stylesheet' href='web-stories-wp/assets/css/edit-story.css'/>
 	<style>
 		body.js.edit-story #web-stories-editor {
 			height: 100vh;
@@ -23,47 +18,54 @@
 	</style>
 
 	<script id='web-stories-tracking-js-after'>
-      window.webStoriesTrackingSettings = {"trackingAllowed":false,"trackingId":"UA-168571240-1","trackingIdGA4":"G-T88C9951CM","appVersion":"1.7.0-alpha.0","userProperties":{"siteLocale":"en_US","userLocale":"en_US","userRole":"administrator","enabledExperiments":"enableQuickTips,customMetaBoxes,videoOptimization","wpVersion":"5.6.3","phpVersion":"8.0.3","isMultisite":0,"adNetwork":"admanager","analytics":false,"activePlugins":""}};
+      window.webStoriesTrackingSettings = {
+        'trackingAllowed': false,
+        'trackingId': 'UA-168571240-1',
+        'trackingIdGA4': 'G-T88C9951CM',
+        'appVersion': '1.7.0-alpha.0',
+        'userProperties': {
+          'siteLocale': 'en_US',
+          'userLocale': 'en_US',
+          'userRole': 'administrator',
+          'enabledExperiments': 'enableQuickTips,customMetaBoxes,videoOptimization',
+          'wpVersion': '5.6.3',
+          'phpVersion': '8.0.3',
+          'isMultisite': 0,
+          'adNetwork': 'admanager',
+          'analytics': false,
+          'activePlugins': '',
+        },
+      };
 	</script>
 </head>
-<body class="wp-admin wp-core-ui no-js edit-story folded post-php auto-fold admin-bar post-type-web-story branch-5-6 version-5-6-3 admin-color-fresh locale-en-us no-customize-support no-svg">
-<script type="text/javascript">
-  document.body.className = document.body.className.replace('no-js','js');
-</script>
+<body class="edit-story no-js">
 
-<div id="wpwrap">
-	<div>
-		<div id="wpbody" role="main">
-			<div id="wpbody-content">
-				<div class="web-stories-wp">
-					<h1 class="screen-reader-text hide-if-no-js">Web Stories</h1>
-					<div id="web-stories-editor" class="web-stories-editor-app-container hide-if-no-js">
-						<h1 class="loading-message">Please wait...</h1>
-					</div>
+	<script type="text/javascript">
+	  document.body.className = document.body.className.replace( 'no-js', 'js' );
+	</script>
 
-					<div class="wrap hide-if-js web-stories-wp-no-js">
-						<h1 class="wp-heading-inline">Web Stories</h1>
-						<div class="notice notice-error notice-alt">
-							<p>Web Stories Ad requires JavaScript. Please enable JavaScript in your browser settings.</p>
-						</div>
-					</div>
-				</div>
+	<div class="web-stories-wp">
+		<h1 class="screen-reader-text hide-if-no-js">Web Stories</h1>
+		<div id="web-stories-editor" class="web-stories-editor-app-container hide-if-no-js">
+			<h1 class="loading-message">Please wait...</h1>
+		</div>
 
-				<div class="clear"></div>
-			</div><!-- wpbody-content -->
-			<div class="clear"></div>
-		</div><!-- wpbody -->
-		<div class="clear"></div>
-	</div><!-- wpcontent -->
+		<div class="wrap hide-if-js web-stories-wp-no-js">
+			<h1 class="wp-heading-inline">Web Stories</h1>
+			<div class="notice notice-error notice-alt">
+				<p>Web Stories Ad requires JavaScript. Please enable JavaScript in your browser settings.</p>
+			</div>
+		</div>
+	</div>
 
-	<script src='https://wordpress.app/wp-admin/load-scripts.php?c=0&amp;load%5Bchunk_0%5D=hoverIntent,wp-i18n&amp;ver=5.6.3'></script>
+	<script src='web-stories-ad/assets/wp-assets/wp-polyfill.min.js'></script>
+	<script src='web-stories-ad/assets/wp-assets/load-scripts.js'></script> <!-- https://wordpress.app/wp-admin/load-scripts.php?load[chunk_0]=wp-i18n -->
 
-	<script src='https://wordpress.app/wp-includes/js/dist/url.min.js?ver=98645f0502e5ed8dadffd161e39072d2' id='wp-url-js'></script>
-	<script src='https://wordpress.app/wp-includes/js/dist/api-fetch.min.js?ver=4dec825c071b87c57f687eb90f7c23c3' id='wp-api-fetch-js'></script>
-	<script src='https://wordpress.app/wp-content/plugins/web-stories-wp/assets/js/vendors-edit-story-stories-dashboard-f6a93713134bae1f571e.js' id='vendors-edit-story-stories-dashboard-f6a93713134bae1f571e-js'></script>
-	<script src='https://wordpress.app/wp-content/plugins/web-stories-wp/assets/js/vendors-edit-story-62b505bcefb44a0adf05.js' id='vendors-edit-story-62b505bcefb44a0adf05-js'></script>
-	<script src='https://wordpress.app/wp-content/plugins/web-stories-wp/assets/js/edit-story-stories-dashboard-a0dd4de352b608d6cd9b.js' id='edit-story-stories-dashboard-a0dd4de352b608d6cd9b-js'></script>
-	<script src='https://wordpress.app/wp-content/plugins/web-stories-wp/assets/js/edit-story.js?ver=d1683a0c1cc0480a1798f1da20f3b018' id='edit-story-js'></script>
-</div><!-- wpwrap -->
+	<script src='web-stories-ad/assets/wp-assets/url.min.js'></script>
+	<script src='web-stories-ad/assets/wp-assets/api-fetch.min.js'></script>
+	<script src='https://wordpress.app/wp-content/plugins/web-stories-wp/assets/js/vendors-edit-story-stories-dashboard-f6a93713134bae1f571e.js'></script>
+	<script src='https://wordpress.app/wp-content/plugins/web-stories-wp/assets/js/vendors-edit-story-62b505bcefb44a0adf05.js'></script>
+	<script src='https://wordpress.app/wp-content/plugins/web-stories-wp/assets/js/edit-story-stories-dashboard-a0dd4de352b608d6cd9b.js'></script>
+	<script src='https://wordpress.app/wp-content/plugins/web-stories-wp/assets/js/edit-story.js?ver=d1683a0c1cc0480a1798f1da20f3b018'></script>
 </body>
 </html>
